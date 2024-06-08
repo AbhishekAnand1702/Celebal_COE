c2c3f3b2d475d7a1c8d1fc7d81eb46f1fc342c12
Abhishek Anand <115548235+AbhishekAnand1702@users.noreply.github.com>
2024-06-08 15:15:26 +0530

    Add files via upload

---
+++ Week - 01 Assignment.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 15
+rows = 5  
+for i in range(1, rows + 1):
+    spaces = rows - i
+    print(' ' * spaces, end='')
+    print('* ' * i)
+
+
+# In[ ]:
+
+
+
+
